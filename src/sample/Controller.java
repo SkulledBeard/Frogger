506c3cdd1ed3577cb51d82c77c30deb5bc991c98
SkulledBeard <the_rest_in_person@hotmail.com>
2021-09-09 14:55:37 +0200

    Update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,8 +2,10 @@ package sample;
 
 
 import javafx.animation.PathTransition;
-import javafx.animation.Transition;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -12,8 +14,11 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
+import java.util.Timer;
+
 
 public class Controller {
+    private int leben = 5;
 
     // Dieses Objekt kommt aus der FXML-Datei
     // @FXML
@@ -22,26 +27,65 @@ public class Controller {
     @FXML
     private Pane game;
 
-    private ImageView carR;
-    private ImageView carL;
+    private Timer timer;
+
+    private ImageView carR, carR2;
+    private ImageView carL, carL2;
+    private ImageView log1, log2, log3,log4;
+
+    private ImageView heart1, heart2, heart3, heart4, heart5;
 
     private Path path = new Path();
     private PathTransition pathTransition = new PathTransition();
     private Path path2 = new Path();
     private PathTransition pathTransition2 = new PathTransition();
+    private Path path3 = new Path();
+    private PathTransition pathTransition3 = new PathTransition();
+    private Path path4 = new Path();
+    private PathTransition pathTransition4 = new PathTransition();
 
     @FXML
     private void initialize(){
         frog = new Avatar("image/frog_50_38_lila.png");
         carR = new ImageView("image/car_green_40_r.png");
         carL = new ImageView("image/car_red_40.png");
+        carR2 = new ImageView("image/car_green_40_r.png");
+        carL2 = new ImageView("image/car_red_40.png");
+        log1 = new ImageView("image/woodlog.png");
+        log2 = new ImageView("image/woodlog.png");
+        log3 = new ImageView("image/woodlog.png");
+        log4 = new ImageView("image/woodlog.png");
+        heart1 = new ImageView("image/heart.png");
+        heart2 = new ImageView("image/heart.png");
+        heart3 = new ImageView("image/heart.png");
+        heart4 = new ImageView("image/heart.png");
+        heart5 = new ImageView("image/heart.png");
 
         game.getChildren().add(frog);
         game.getChildren().add(carR);
         game.getChildren().add(carL);
+        game.getChildren().add(carR2);
+        game.getChildren().add(carL2);
+        game.getChildren().add(log1);
+        game.getChildren().add(log2);
+        game.getChildren().add(log3);
+        game.getChildren().add(log4);
+        game.getChildren().add(heart1);
+        game.getChildren().add(heart2);
+        game.getChildren().add(heart3);
+        game.getChildren().add(heart4);
+        game.getChildren().add(heart5);
+
+        heart1.setX(10);
+        heart2.setX(70);
+        heart3.setX(130);
+        heart4.setX(190);
+        heart5.setX(250);
 
         carRTransition();
         carLTransition();
+        carR2Transition();
+        carL2Transition();
 
         System.out.println(frog.getX());
         System.out.println(frog.getY());
@@ -49,10 +93,12 @@ public class Controller {
 
 
         frog.setFocusTraversable(true);
-        if (carR.intersects(frog.getBoundsInParent()) || carL.intersects(frog.getBoundsInParent())) {
-            collision();
-        }
 
+        carL.translateXProperty().addListener(checkIntersection2);
+        carL.translateYProperty().addListener(checkIntersection2);
+
+        carR.translateXProperty().addListener(checkIntersection);
+        carR.translateYProperty().addListener(checkIntersection);
         // Eventhandler
         frog.addEventHandler(KeyEvent.KEY_PRESSED,keyEvent ->{
             switch(keyEvent.getCode()){
@@ -94,16 +140,31 @@ public class Controller {
 
     public void carRTransition() {
 
-        MoveTo moveTo = new MoveTo(-50, 525);
-        LineTo line1 = new LineTo(900, 525);
+        MoveTo moveTo = new MoveTo(-50, 680);
+        LineTo line1 = new LineTo(900, 680);
         path.getElements().add(moveTo);
         path.getElements().add(line1);
-        pathTransition.setDuration(Duration.millis(1500));
+        pathTransition.setDuration(Duration.millis(1235));
         pathTransition.setNode(carR);
         pathTransition.setCycleCount(PathTransition.INDEFINITE);
         pathTransition.setAutoReverse(false);
         pathTransition.setPath(path);
         pathTransition.play();
+
+    }
+    public void carR2Transition() {
+
+        MoveTo moveTo = new MoveTo(-50, 525);
+        LineTo line1 = new LineTo(900, 525);
+        path3.getElements().add(moveTo);
+        path3.getElements().add(line1);
+        pathTransition3.setDuration(Duration.millis(1500));
+        pathTransition3.setNode(carR2);
+        pathTransition3.setCycleCount(PathTransition.INDEFINITE);
+        pathTransition3.setAutoReverse(false);
+        pathTransition3.setPath(path3);
+        pathTransition3.play();
+
     }
 
     public void carLTransition() {
@@ -112,11 +173,64 @@ public class Controller {
         LineTo line1 = new LineTo(-50, 640);
         path2.getElements().add(moveTo);
         path2.getElements().add(line1);
-        pathTransition2.setDuration(Duration.millis(1500));
+        pathTransition2.setDuration(Duration.millis(2132));
         pathTransition2.setNode(carL);
         pathTransition2.setCycleCount(PathTransition.INDEFINITE);
         pathTransition2.setAutoReverse(false);
         pathTransition2.setPath(path2);
         pathTransition2.play();
+
+    }
+
+    public void carL2Transition() {
+
+        MoveTo moveTo = new MoveTo(900, 475);
+        LineTo line1 = new LineTo(-50, 475);
+        path4.getElements().add(moveTo);
+        path4.getElements().add(line1);
+        pathTransition4.setDuration(Duration.millis(1500));
+        pathTransition4.setNode(carL2);
+        pathTransition4.setCycleCount(PathTransition.INDEFINITE);
+        pathTransition4.setAutoReverse(false);
+        pathTransition4.setPath(path4);
+        pathTransition4.play();
+
+    }
+
+    // Prüft ob Autos mit Frosch kollidieren
+    private final ChangeListener<Number> checkIntersection = (ob,n,n1)->{
+        if (carR.getBoundsInParent().intersects(frog.getBoundsInParent())){
+            endgame();
+        }
+    };
+
+    private final ChangeListener<Number> checkIntersection2 = (ob,n,n1)->{
+        if (carL.getBoundsInParent().intersects(frog.getBoundsInParent())){
+            endgame();
+
+        }
+    };
+
+    private void endgame() {
+        System.out.println("Ein Leben Verloren");
+        frog.setX(385);
+        frog.setY(725);
+        leben -= 1;            if (leben == 4) {game.getChildren().remove(heart5);}
+        if (leben == 3) {game.getChildren().remove(heart4);}
+        if (leben == 2) {game.getChildren().remove(heart3);}
+        if (leben == 1) {game.getChildren().remove(heart2);}
+        if (leben == 0) {
+            game.getChildren().remove(heart1);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Game Over");
+            alert.setHeaderText("Game Over!");
+            alert.setContentText("You have 0 hearts left");
+            pathTransition.stop();
+            pathTransition2.stop();
+            alert.setOnHidden(evt -> Platform.exit());
+
+            alert.show();
+        }
     }
 }
+
